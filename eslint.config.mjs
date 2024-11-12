12c566b03c3553c6592903528f1a45885e0f6ecf
svitlana1dev <ponomarchuk.svitlana.dev@gmail.com>
2024-11-12 15:23:18 +0200

    added setups

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "no-console": "warn",
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
